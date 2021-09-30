2fdf951a8aff5b1fad411a5a0714d01a44231bf1
Hatim <hatimjuzer78652@gmail.com>
2021-09-30 18:36:00 +0500

    Removed comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-// import TodoItem from './todoItems';
 import AddTodo from './addTodo';
-// import API from '../api/api';
-// import { addNewTodo, removeTodo, toggleComplete } from '../api/helpers';
 import store from '../redux/store';
 import TodoItems from './TodoItems';
 
 function App() {
-  // const onRemoveClick = (id) => {
-  //   removeTodo(id)
-  //     .then(() => {
-  //       setItems(items.filter((elem) => elem.id !== id));
-  //     });
-  // };
-
-  // const onAddTodo = (todoText) => {
-  //   const newTodo = { complete: false, todo: todoText };
-  //   addNewTodo(newTodo)
-  //     .then((res) => {
-  //       setItems([res.data, ...items]);
-  //     });
-  // };
-
   return (
     <Provider store={store}>
       <AddTodo />
